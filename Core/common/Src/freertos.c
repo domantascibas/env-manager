f6d015ca1a811c7b7605059b3e801b7afe231406
Domantas Cibas <domantas@nectarsun.com>
2021-03-01 15:29:28 +0200

    led wip

--- Core/common/Src/freertos.c
+++ Core/common/Src/freertos.c
@@ -3,33 +3,22 @@
 #include "main.h"
 #include "cmsis_os.h"
 
-osThreadId_t defaultTaskHandle;
+#include "led.h"
 
-const osThreadAttr_t defaultTask_attributes = {
-    .name = "defaultTask",
-    .stack_size = 128 * 4,
-    .priority = (osPriority_t) osPriorityNormal,
-};
-
-void StartDefaultTask(void *argument);
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+void vLedAliveTask(void *pvParameters);
 
-/**
-  * @brief  FreeRTOS initialization
-  * @param  None
-  * @retval None
-  */
 void MX_FREERTOS_Init(void) {
-    defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+    xTaskCreate(vLedAliveTask, "LedAlive", 128, NULL, 1, NULL);
 }
 
-/**
-  * @brief  Function implementing the defaultTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-void StartDefaultTask(void *argument) {
-    for(;;) {
-        osDelay(1);
+void vLedAliveTask(void *pvParameters) {
+    portTickType xLastWakeTime;
+    const portTickType xFrequency = 1000;
+    xLastWakeTime = xTaskGetTickCount();
+
+    for (;;) {
+        led_toggle(LED_RED);
+        vTaskDelayUntil(&xLastWakeTime, xFrequency);
     }
 }
---
+++ Core/modules/led/led.h
@@ -0,0 +1,16 @@
+#ifndef LED_H
+#define LED_H
+
+#include "stm32f103xb.h"
+#include "stm32f1xx_hal_gpio.h"
+
+#define LED_PORT GPIOB
+#define LED_RED GPIO_PIN_9
+#define LED_YEL GPIO_PIN_8
+
+void led_init(void);
+void led_on(uint16_t led);
+void led_off(uint16_t led);
+void led_toggle(uint16_t led);
+
+#endif
