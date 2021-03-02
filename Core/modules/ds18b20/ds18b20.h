4e7c2ebb794300114d2e211f3e6f6736f09e0359
Domantas Cibas <domantas@nectarsun.com>
2021-03-02 11:15:07 +0200

    reset whole project

--- Core/modules/ds18b20/ds18b20.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DS18B20_H_
-#define _DS18B20_H_
-
-#include "stdint.h"
-
-#define DS18B20 1
-
-#endif
--- project/module.mk
+++
@@ -1,7 +0,0 @@
-MODULE_PATH = ../Core/modules
-MODULE_DIRS = $(patsubst $(MODULE_PATH)/%, %, $(sort \
-	$(dir $(wildcard $(MODULE_PATH)/*/)) \
-	))
-
-C_SOURCES += $(wildcard $(patsubst %, $(MODULE_PATH)/%*.c, $(MODULE_DIRS)))
-C_INCLUDES += $(patsubst %, -I$(MODULE_PATH)/%, $(MODULE_DIRS))
