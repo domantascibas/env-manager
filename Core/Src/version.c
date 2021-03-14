f819b0a284659c40ddbe7abba54749e3db6739ae
Domantas Cibas <domantas@nectarsun.com>
2021-03-14 23:22:09 +0200

    #00.0A.00.04

--- Core/Src/version.c
+++ Core/Src/version.c
@@ -4,7 +4,7 @@
 #include "stdint.h"
 #include "stdio.h"
 
-#define RELEASE_VER             "00.0A.00.03"
+#define RELEASE_VER             "00.0A.00.04"
 
 const char version[VERSION_LENGTH] = RELEASE_VER;
 
