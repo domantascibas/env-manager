8ab2d2b4652a0f7d9374c86e474e09de0cc88bb9
Domantas Cibas <domantas@nectarsun.com>
2021-03-15 11:59:30 +0200

    task manager update

--- Core/Inc/task_manager.h
+++ Core/Inc/task_manager.h
@@ -1,7 +1,33 @@
 #ifndef _TASK_MANAGER_H_
 #define _TASK_MANAGER_H_
 
-void task_manager_init(void);
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define SIZEOF(arr) (sizeof(arr) / sizeof(arr[0]))
+
+typedef enum {
+    taskINIT = 0,       // init system - modules and other
+    taskUartTx,
+    taskUartRx,
+    taskTskManager,
+    taskStatusLed,
+    taskPrintBlink,
+} eTaskID;
+
+typedef struct {
+    eTaskID const hardID;
+    const char * const name;
+    TaskFunction_t func;
+    TaskHandle_t * const handle;
+    void * const parameters;
+    const configSTACK_DEPTH_TYPE stack_size;
+    UBaseType_t priority;
+    uint8_t autorun;
+} taskDescription_t;
+
+void taskStart(eTaskID id);
+void taskStop(eTaskID id);
 void task_toggle(void);
 
 #endif
--- Core/Src/task_manager.c
+++ Core/Src/task_manager.c
@@ -1,5 +1,3 @@
-#include "FreeRTOS.h"
-#include "task.h"
 #include "task_manager.h"
 #include "uart.h"
 
@@ -8,44 +6,73 @@
 static const char moduleStr[] = "TSK";
 #define PTS_dbg(fmt, ...) PTS_d(moduleStr, fmt, ##__VA_ARGS__)
 #define PTS_dbg_f(fmt, ...) PTS_df(moduleStr, fmt, ##__VA_ARGS__)
-#define SIZEOF(arr) (sizeof(arr) / sizeof(arr[0]))
+
+static xTaskHandle _hInit;
+// static xTaskHandle _hUartTx;
+// static xTaskHandle _hUartRx;
+static xTaskHandle _hTaskManager;
+static xTaskHandle _hStatusLed;
+static xTaskHandle _hPrintBlink;
 
 void _tTaskManager(void *argument);
 
-xTaskHandle _hTaskManager;
-xTaskHandle _hStatusLed;
-xTaskHandle _hPrintBlink;
-
-typedef struct {
-    const char * const name;
-    TaskFunction_t func;
-    TaskHandle_t * const handle;
-    void * const parameters;
-    const configSTACK_DEPTH_TYPE stack_size;
-    UBaseType_t priority;
-    uint8_t autorun;
-} taskStruct_t;
-
-const static taskStruct_t tasks[] = {
-    {"statusLed",           _tStatusLed,        &_hStatusLed,        NULL,       configMINIMAL_STACK_SIZE,       16,        1},
-    {"printBlink",          _tPrintBlink,       &_hPrintBlink,       NULL,       configMINIMAL_STACK_SIZE,       16,        0},
+static const taskDescription_t tasks[] = {
+    // {taskINIT,              "taskInit",             _tInit,             &_hInit,                NULL,       configMINIMAL_STACK_SIZE,           16,         1},
+    // {taskUartTx,            "taskUartTx",           _tUartTx,           &_hUartTx,              NULL,       configMINIMAL_STACK_SIZE,           16,         1},
+    // {taskUartRx,            "taskUartRx",           _tUartRx,           &_hUartRx,              NULL,       configMINIMAL_STACK_SIZE * 2,       16,         1},
+    {taskTskManager,        "taskManager",          _tTaskManager,      &_hTaskManager,         NULL,       configMINIMAL_STACK_SIZE,           16,         1},
+    {taskStatusLed,         "statusLed",            _tStatusLed,        &_hStatusLed,           NULL,       configMINIMAL_STACK_SIZE,           16,         1},
+    {taskPrintBlink,        "printBlink",           _tPrintBlink,       &_hPrintBlink,          NULL,       configMINIMAL_STACK_SIZE,           16,         0},
 };
 
-void task_manager_init(void) {
-    xTaskCreate(_tTaskManager, "taskManager", configMINIMAL_STACK_SIZE, NULL, 16, &_hTaskManager);
+int8_t getTaskIndex(eTaskID id) {
+    uint8_t i;
+    for (i = 0; i < SIZEOF(tasks); i++) {
+        if (tasks[i].hardID == id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+const taskDescription_t* findTask(eTaskID id) {
+    int8_t idx = getTaskIndex(id);
+    if (idx >= 0) {
+        return &tasks[idx];
+    } else {
+        PTS_dbg_f(" unknown task ID: %d", id);
+    }
+    return NULL;
+}
+
+void taskStart(eTaskID id) {
+    const taskDescription_t *task = findTask(id);
+    if (task != NULL) {
+        xTaskCreate(task->func, task->name, task->stack_size, task->parameters, task->priority, task->handle);
+        PTS_dbg_f(" +%-16s START", task->name);
+    }
+}
+
+void taskStop(eTaskID id) {
+    const taskDescription_t *task = findTask(id);
+    if (task != NULL) {
+        vTaskDelete(*task->handle);
+        PTS_dbg_f(" -%-16s STOP", task->name);
+    }
 }
 
 void _tTaskManager(void *argument) {
-    uint8_t i;
-    const taskStruct_t* task;
+    taskStart(taskStatusLed);
+    // uint8_t i;
+    // const taskDescription_t* task;
 
-    for (i = 0; i < SIZEOF(tasks); i++) {
-        task = &tasks[i];
-        if (task->autorun) {
-            xTaskCreate(task->func, task->name, task->stack_size, task->parameters, task->priority, task->handle);
-            PTS_dbg_f(" +%-16s START", task->name);
-        }
-    }
+    // for (i = 0; i < SIZEOF(tasks); i++) {
+    //     task = &tasks[i];
+    //     if (task->autorun) {
+    //         xTaskCreate(task->func, task->name, task->stack_size, task->parameters, task->priority, task->handle);
+    //         PTS_dbg_f(" +%-16s START", task->name);
+    //     }
+    // }
 
     // while(1) {
     //     /* print some task statistics */
@@ -55,7 +82,7 @@ void _tTaskManager(void *argument) {
 }
 
 void task_toggle(void) {
-    const taskStruct_t* task = &tasks[1];
+    const taskDescription_t* task = &tasks[1];
     // PTS_dbg_f(" %s-16s handle:%d", task->name, *task->handle);
 
     if (*task->handle == 0) {
